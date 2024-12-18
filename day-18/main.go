6d376e7abc2ce3105da9c99a31cb1d7ff28c6e37
Oleg Sidorov <oleg@sidorov.nl>
2024-12-18 07:34:33 +0100

    Implement day 18

---
+++ day-18/main.go
@@ -0,0 +1,93 @@
+package main
+
+import "fmt"
+
+func readPoint(s string) Point2 {
+	var p Point2
+	_, err := fmt.Sscanf(s, "%d,%d", &p.x, &p.y)
+	noerr(err)
+	return p
+}
+
+const (
+	OBST = 1
+)
+
+func shortestPath(S, E Point2, B []Point2) int {
+	F := makeIntField(E.y+1, E.x+1)
+	Q := make([]Point3, 0, 1)
+	V := make(map[Point2]int)
+	Q = append(Q, Point3{S.x, S.y, 0})
+
+	for _, b := range B {
+		F[b.y][b.x] = OBST
+	}
+
+	// fmt.Println(printNumFieldWithSubs(F, "", map[int]string{
+	// 	0: ".",
+	// 	1: "#",
+	// }))
+
+	minpath := ALOT
+	var H Point3
+	for len(Q) > 0 {
+		H, Q = Q[0], Q[1:]
+		debugf("H: %+v", H)
+		H2 := Point2{H.x, H.y}
+		if prev, ok := V[H2]; ok && prev <= H.z {
+			continue
+		}
+		V[H2] = H.z
+		if H.x == E.x && H.y == E.y {
+			debugf("reached end: %d", H.z)
+			minpath = min(minpath, H.z)
+			continue
+		}
+		for _, step := range STEPS4 {
+			nx, ny := H.x+step[0], H.y+step[1]
+			if nx < 0 || nx >= len(F[0]) || ny < 0 || ny >= len(F) {
+				continue
+			}
+			if F[ny][nx] == OBST {
+				continue
+			}
+			NP2 := Point2{nx, ny}
+			NP3 := Point3{nx, ny, H.z + 1}
+			if prev, ok := V[NP2]; !ok || prev > NP3.z {
+				Q = append(Q, NP3)
+			}
+		}
+	}
+
+	return minpath
+}
+
+func main() {
+	lines := input()
+
+	S := Point2{0, 0}
+	ix := 0
+	E := readPoint(lines[ix])
+	ix++
+	nBytes := parseInt(lines[ix])
+	ix += 2
+
+	B := make([]Point2, 0, len(lines)-3)
+	for ix < len(lines) {
+		B = append(B, readPoint(lines[ix]))
+		ix++
+	}
+	debugf("B: %+v", B)
+
+	res1 := shortestPath(S, E, B[:nBytes])
+	printf("res1: %d", res1)
+
+	for i := 1; i < len(B); i++ {
+		v := shortestPath(S, E, B[:nBytes+i])
+		if v == ALOT {
+			lastb := B[nBytes+i-1]
+			printf("res2: %d,%d", lastb.x, lastb.y)
+			break
+		}
+	}
+}
